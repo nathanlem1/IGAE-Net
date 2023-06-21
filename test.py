29e0a0c99c609b40213d99876930380877eb2758
nathanlem1 <nathanaellmss@gmail.com>
2023-06-21 22:14:52 +0100

    Adding train.py and test.py

---
+++ test.py
@@ -0,0 +1,267 @@
+import argparse
+import os
+import sys
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from dataset_11k import HandsDataset, AttributesDataset, mean, std
+from model.models import IGAE_ResNet50, IGAE_DenseNet121, IGAE_ConvNext_t, IGAE_vit_b_16, IGAE_swin_t, IGAE_maxvit_t
+from get_loss import get_loss
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, balanced_accuracy_score, classification_report
+from torch.utils.data import DataLoader
+
+
+def checkpoint_load(model, name):
+    print('Restoring checkpoint: {}'.format(name))
+    model.load_state_dict(torch.load(name, map_location='cpu'))
+    epoch = int(os.path.splitext(os.path.basename(name))[0].split('-')[1])
+    return epoch
+
+
+def validate(model, dataloader, logger, iteration, device, checkpoint=None):
+    if checkpoint is not None:
+        checkpoint_load(model, checkpoint)
+
+    model.eval()
+    with torch.no_grad():
+        avg_loss = 0
+        accuracy_identity = 0
+        accuracy_gender = 0
+        accuracy_age = 0
+
+        for batch in dataloader:
+            img = batch['img']
+            target_labels = batch['labels']
+            target_labels = {t: target_labels[t].to(device) for t in target_labels}
+            output = model(img.to(device))
+
+            val_train, val_train_losses = get_loss(output, target_labels)
+            avg_loss += val_train.item()
+            batch_accuracy_identity, batch_accuracy_gender, batch_accuracy_age = \
+                calculate_metrics(output, target_labels)
+
+            accuracy_identity += batch_accuracy_identity
+            accuracy_gender += batch_accuracy_gender
+            accuracy_age += batch_accuracy_age
+
+    n_samples = len(dataloader)
+    avg_loss /= n_samples
+    accuracy_identity /= n_samples
+    accuracy_gender /= n_samples
+    accuracy_age /= n_samples
+    print('-' * 72)
+    print("Validation  loss: {:.4f}, identity: {:.4f}, gender: {:.4f}, age: {:.4f}\n".format(
+        avg_loss, accuracy_identity, accuracy_gender, accuracy_age))
+
+    logger.add_scalar('val_loss', avg_loss, iteration)
+    logger.add_scalar('val_accuracy_identity', accuracy_identity, iteration)
+    logger.add_scalar('val_accuracy_gender', accuracy_gender, iteration)
+    logger.add_scalar('val_accuracy_age', accuracy_age, iteration)
+
+    model.train()
+
+    return accuracy_identity, accuracy_gender, accuracy_age
+
+
+def visualize_grid(model, dataloader, attributes, device, show_cn_matrices=True, show_images=True, checkpoint=None,
+                   show_gt=False):
+    if checkpoint is not None:
+        checkpoint_load(model, checkpoint)
+    model.eval()
+
+    imgs = []
+    labels = []
+    gt_labels = []
+    gt_identity_all = []
+    gt_gender_all = []
+    gt_age_all = []
+    predicted_identity_all = []
+    predicted_gender_all = []
+    predicted_age_all = []
+
+    accuracy_identity = 0
+    accuracy_gender = 0
+    accuracy_age = 0
+
+    with torch.no_grad():
+        for batch in dataloader:
+            img = batch['img']
+            gt_identities = batch['labels']['identity_labels']
+            gt_genders = batch['labels']['gender_labels']
+            gt_ages = batch['labels']['age_labels']
+            output = model(img.to(device))
+
+            batch_accuracy_identity, batch_accuracy_gender, batch_accuracy_age = \
+                calculate_metrics(output, batch['labels'])
+            accuracy_identity += batch_accuracy_identity
+            accuracy_gender += batch_accuracy_gender
+            accuracy_age += batch_accuracy_age
+
+            # get the most confident prediction for each image
+            _, predicted_identities = output['identity'].cpu().max(1)
+            _, predicted_genders = output['gender'].cpu().max(1)
+            _, predicted_ages = output['age'].cpu().max(1)
+
+            for i in range(img.shape[0]):
+                image = np.clip(img[i].permute(1, 2, 0).numpy() * std + mean, 0, 1)
+
+                predicted_identity = attributes.identity_id_to_name[predicted_identities[i].item()]
+                predicted_gender = attributes.gender_id_to_name[predicted_genders[i].item()]
+                predicted_age = attributes.age_id_to_name[predicted_ages[i].item()]
+
+                gt_identity = attributes.identity_id_to_name[gt_identities[i].item()]
+                gt_gender = attributes.gender_id_to_name[gt_genders[i].item()]
+                gt_age = attributes.age_id_to_name[gt_ages[i].item()]
+
+                gt_identity_all.append(gt_identity)
+                gt_gender_all.append(gt_gender)
+                gt_age_all.append(gt_age)
+
+                predicted_identity_all.append(predicted_identity)
+                predicted_gender_all.append(predicted_gender)
+                predicted_age_all.append(predicted_age)
+
+                imgs.append(image)
+                labels.append("{}\n{}\n{}".format(predicted_identity, predicted_gender, predicted_age))
+                gt_labels.append("{}\n{}\n{}".format(gt_identity, gt_gender, gt_age))
+
+    # Show classification accuracy and save it
+    n_samples = len(dataloader)
+    print("\nAccuracy:\nidentity: {:.4f}, gender: {:.4f}, age: {:.4f}".format(
+        accuracy_identity / n_samples,
+        accuracy_gender / n_samples,
+        accuracy_age / n_samples))
+    result = os.path.join('/'.join(checkpoint.split('/')[:-1]), checkpoint.split('/')[-1][:-4] + '_accuracy.txt')
+    res = open(result, 'w')
+    res.write('identity:%.4f gender:%.4f age:%.4f' % (accuracy_identity / n_samples, accuracy_gender / n_samples,
+                                                          accuracy_age / n_samples))
+
+    # Show classification report
+    with warnings.catch_warnings():  # sklearn may produce a warning when processing zero row in classification report
+        warnings.simplefilter("ignore")
+        print('\nIdentity classification report: \n', classification_report(gt_identity_all, predicted_identity_all))
+        print('Gender classification report: \n', classification_report(gt_gender_all, predicted_gender_all))
+        print('Age classification report: \n', classification_report(gt_age_all, predicted_age_all))
+
+    # Draw confusion matrices
+    if show_cn_matrices:
+        # Identity
+        cn_matrix = confusion_matrix(
+            y_true=gt_identity_all,
+            y_pred=predicted_identity_all,
+            labels=attributes.identity_labels,
+            normalize='true')
+        ConfusionMatrixDisplay(cn_matrix, attributes.identity_labels).plot(
+            include_values=False, xticks_rotation='vertical')
+        plt.title("Identity")
+        plt.tight_layout()
+        plt.savefig(os.path.join('/'.join(checkpoint.split('/')[:-1]), 'Identity_cm.png'))
+        plt.show()
+
+        # gender
+        cn_matrix = confusion_matrix(
+            y_true=gt_gender_all,
+            y_pred=predicted_gender_all,
+            labels=attributes.gender_labels,
+            normalize='true')
+        ConfusionMatrixDisplay(cn_matrix, attributes.gender_labels).plot(
+            xticks_rotation='horizontal')
+        plt.title("Gender")
+        plt.tight_layout()
+        plt.savefig(os.path.join('/'.join(checkpoint.split('/')[:-1]), 'Gender_cm.png'))
+        plt.show()
+
+        # Uncomment code below to see the age confusion matrix (it may be too big to display)
+        cn_matrix = confusion_matrix(
+            y_true=gt_age_all,
+            y_pred=predicted_age_all,
+            labels=attributes.age_labels,
+            normalize='true')
+        # plt.rcParams.update({'font.size': 1.8})
+        # plt.rcParams.update({'figure.dpi': 300})
+        ConfusionMatrixDisplay(cn_matrix, attributes.age_labels).plot(
+            include_values=True, xticks_rotation='vertical')
+        # plt.rcParams.update({'figure.dpi': 100})
+        # plt.rcParams.update({'font.size': 5})
+        plt.title("Age")
+        plt.savefig(os.path.join('/'.join(checkpoint.split('/')[:-1]), 'Age_cm.png'))
+        plt.show()
+
+    if show_images:
+        title = "Ground truth labels vs Predicted labels"
+        n_cols = 5
+        n_rows = 3
+        fig, axs = plt.subplots(n_rows, n_cols, figsize=(10, 10))
+        axs = axs.flatten()
+        for img, ax, gt_label, pr_label in zip(imgs, axs, gt_labels, labels):
+            ax.set_xlabel('GT: ' + gt_label + "\n " + 'PR: ' + pr_label, rotation=0)
+            ax.get_xaxis().set_ticks([])
+            ax.get_yaxis().set_ticks([])
+            ax.imshow(img)
+        plt.suptitle(title)
+        plt.tight_layout()
+        plt.savefig(os.path.join('/'.join(checkpoint.split('/')[:-1]), 'sample_result.png'))
+        plt.show()
+
+    model.train()
+
+
+def calculate_metrics(output, target):
+    _, predicted_identity = output['identity'].cpu().max(1)
+    gt_identity = target['identity_labels'].cpu()
+
+    _, predicted_gender = output['gender'].cpu().max(1)
+    gt_gender = target['gender_labels'].cpu()
+
+    _, predicted_age = output['age'].cpu().max(1)
+    gt_age = target['age_labels'].cpu()
+
+    with warnings.catch_warnings():  # sklearn may produce a warning when processing zero row in confusion matrix
+        warnings.simplefilter("ignore")
+        accuracy_identity = balanced_accuracy_score(y_true=gt_identity.numpy(), y_pred=predicted_identity.numpy())
+        accuracy_gender = balanced_accuracy_score(y_true=gt_gender.numpy(), y_pred=predicted_gender.numpy())
+        accuracy_age = balanced_accuracy_score(y_true=gt_age.numpy(), y_pred=predicted_age.numpy())
+
+    return accuracy_identity, accuracy_gender, accuracy_age
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Inference pipeline')
+    parser.add_argument('--checkpoint', type=str, default='./checkpoints/2023-06-21_14-21/checkpoint-000030-best.pth',
+                        help="Path to the checkpoint")
+    parser.add_argument('--attributes_file', type=str, default='./11k/sub_dataset/dorsal_dr.csv',
+                        help="Path to the file with attributes")
+
+    args = parser.parse_args()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    attributes = AttributesDataset(args.attributes_file)  # attributes variable contains labels for the categories in
+    # the dataset and mapping between string names and IDs.
+
+    # During validation, we use only tensor and normalization transforms
+    val_transform = transforms.Compose([
+        transforms.Resize((224, 224), transforms.InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+
+    sub_dataset = os.path.split(args.attributes_file)[1].split('.')[0]
+    test_dataset = HandsDataset(os.path.join('./11k/trainval', sub_dataset, 'val.csv'), attributes, val_transform)
+    test_dataloader = DataLoader(test_dataset, batch_size=20, shuffle=False, num_workers=8)
+
+    model = IGAE_swin_t(n_identity_classes=attributes.num_identities, n_gender_classes=attributes.num_genders,
+                        n_age_classes=attributes.num_ages)
+
+    # Send model to GPU; perform multi-GPU training if more than one GPU is available. It is recommended to use
+    # DistributedDataParallel instead of DataParallel though.
+    if torch.cuda.is_available() and torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)  # Data parallelism
+    model = model.to(device)
+
+    # Visualization of the trained model
+    visualize_grid(model, test_dataloader, attributes, device, checkpoint=args.checkpoint)
+
+    print('Testing is finished!')
---
+++ train.py
@@ -0,0 +1,244 @@
+import argparse
+import copy
+from datetime import datetime
+import os
+import sys
+
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.optim import lr_scheduler
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from dataset_11k import HandsDataset, AttributesDataset, mean, std
+from get_loss import get_loss
+from lr_scheduler import LRScheduler
+from model.models import IGAE_ResNet50, IGAE_DenseNet121, IGAE_ConvNext_t, IGAE_vit_b_16, IGAE_swin_t, IGAE_maxvit_t
+from test import calculate_metrics, validate, visualize_grid
+
+
+def get_cur_time():
+    return datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M')
+
+
+def checkpoint_save(model, name, epoch, best=False):
+    f = os.path.join(name, 'checkpoint-{:06d}.pth'.format(epoch))
+    if best:
+        f = os.path.join(name, 'checkpoint-{:06d}-{:s}.pth'.format(epoch, 'best'))
+    torch.save(model.state_dict(), f)
+    print('Saved checkpoint:', f)
+
+
+# For reproducibility
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    cudnn.deterministic = True
+    cudnn.benchmark = False   # If True, causes cuDNN to benchmark multiple convolution algorithms and select the
+    # fastest. For PyTorch reproducibility, you need to set to False at cost of slightly lower run-time performance
+    # but easy for experimentation.
+    np.random.seed(seed)      # For Numpy reproducibility; this is only used in case any library depends on numpy!
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Training pipeline for IGAE-Net models.')
+    parser.add_argument('--attributes_file', type=str, default='./11k/sub_dataset/palmar_pl.csv',
+                        help="Path to the csv file with attributes")
+    parser.add_argument('--optimizer', default='adam', type=str, help='Optimizer to use: sgd or adam')
+    parser.add_argument('--lr', default=0.0008, type=float,
+                        help='learning rate for new parameters, try 0.015, 0.02, 0.05, 0.001 (adam default)'
+                             'For pretrained parameters, it is 10 times smaller than this')
+    parser.add_argument('--warmup', action='store_true', help='Use warmup learning strategy.')
+    parser.add_argument('--batch_size', default=20, type=int, help='Batch size')  # 10, 20, 32, etc
+    parser.add_argument('--N_epochs', default=50, type=int, help='Number of epochs for training.') # 50, 60,
+    parser.add_argument('--num_workers', default=8, type=int, help='Number of processes to handle dataset loading')
+
+    args = parser.parse_args()
+    args.warmup = True  # Use warmup learning strategy
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # For reproducibility
+    seed = 3  # You can set the seed to any fixed value.
+    set_seed(seed)
+
+    # attributes variable contains labels for the categories in the dataset and mapping between string names and IDs
+    attributes = AttributesDataset(args.attributes_file)
+
+    # Specify image transforms for augmentation during training
+    train_transform = transforms.Compose([
+        transforms.Resize((256, 256), transforms.InterpolationMode.BICUBIC),
+        transforms.RandomCrop((224, 224)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0),
+        # transforms.RandomAffine(degrees=20, translate=(0.1, 0.1), scale=(0.8, 1.2),
+        #                         shear=None, resample=False, fillcolor=(255, 255, 255)),  # It is not helpful!
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std),
+        # transforms.RandomErasing()    # It is not helpful!
+    ])
+
+    # During validation, we use only tensor and normalization transforms
+    val_transform = transforms.Compose([
+        transforms.Resize((224, 224), transforms.InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+
+    sub_dataset = os.path.split(args.attributes_file)[1].split('.')[0]
+    train_dataset = HandsDataset(os.path.join('./11k/trainval', sub_dataset, 'train.csv'), attributes, train_transform)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers,
+                                  pin_memory=True)
+
+    val_dataset = HandsDataset(os.path.join('./11k/trainval', sub_dataset, 'val.csv'), attributes, val_transform)
+    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers,
+                                pin_memory=True)
+
+    model = IGAE_swin_t(n_identity_classes=attributes.num_identities,
+                        n_gender_classes=attributes.num_genders,
+                        n_age_classes=attributes.num_ages)
+
+    # Send model to GPU; perform multi-GPU training if more than one GPU is available. It is recommended to use
+    # DistributedDataParallel instead of DataParallel though.
+    if torch.cuda.is_available() and torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)  # Data parallelism
+    model = model.to(device)
+
+    # Optimizer
+    if hasattr(model, 'backbone'):
+        base_param_ids = set(map(id, model.backbone.parameters()))
+        new_params = [p for p in model.parameters() if
+                      id(p) not in base_param_ids]
+        param_groups = [
+            {'params': filter(lambda p: p.requires_grad, model.backbone.parameters()), 'lr': 0.1*args.lr},
+            {'params': filter(lambda p: p.requires_grad, new_params), 'lr': args.lr}]
+    else:
+        param_groups = model.parameters()
+
+    if args.optimizer == 'sgd':
+        optimizer = torch.optim.SGD(param_groups,
+                                    lr=args.lr,
+                                    momentum=0.9,
+                                    weight_decay=5e-4,
+                                    nesterov=True
+                                    )
+    elif args.optimizer == 'adam':
+        optimizer = torch.optim.Adam(
+            param_groups,
+            lr=args.lr,
+            weight_decay=5e-4
+        )
+    else:
+        raise NameError
+
+    logdir = os.path.join('./logs/', get_cur_time())
+    savedir = os.path.join('./checkpoints/', get_cur_time())
+    os.makedirs(logdir, exist_ok=True)
+    os.makedirs(savedir, exist_ok=True)
+    logger = SummaryWriter(logdir)
+
+    n_train_samples = len(train_dataloader)
+
+    # Uncomment rows below to see example images with ground truth labels in val dataset and all the labels:
+    # visualize_grid(model, val_dataloader, attributes, device, show_cn_matrices=False, show_images=True,
+    #                checkpoint=None, show_gt=True)
+    # print("\nAll identity labels:\n", attributes.identity_labels)
+    # print("\nAll gender labels:\n", attributes.gender_labels)
+    # print("\nAll age labels:\n", attributes.age_labels)
+
+    print(" ----- Starting training ------- ")
+
+    best_accuracy = 0
+    best_epoch = 0
+    average_accuracy = 0
+    each_at_best_accuracy = []
+    best_model_wts = copy.deepcopy(model.state_dict())
+
+    if args.warmup:
+        # Learning rate scheduler
+        lr_scheduler_warmup = LRScheduler(base_lr=0.0008, step=[25, 40],  # Standard (ours)
+                                          factor=0.5, warmup_epoch=10,
+                                          warmup_begin_lr=0.000008)
+    else:
+        # Decay LR by a factor of 0.1 every some (e.g. 30) epochs
+        # lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+        lr_scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[20, 40], gamma=0.1)
+
+    for epoch in range(1, args.N_epochs + 1):
+        total_loss = 0
+        accuracy_identity = 0
+        accuracy_gender = 0
+        accuracy_age = 0
+
+        if args.warmup:
+            lr = lr_scheduler_warmup.update(epoch)  # Warmup strategy is included.
+            optimizer.param_groups[0]['lr'] = 0.1*lr  # For pretrained layers
+            optimizer.param_groups[1]['lr'] = lr  # For new layers
+
+        for batch in train_dataloader:
+            optimizer.zero_grad()  # Zero the parameter gradients
+
+            img = batch['img']
+            target_labels = batch['labels']
+            target_labels = {t: target_labels[t].to(device) for t in target_labels}
+            output = model(img.to(device))
+
+            loss_train, losses_train = get_loss(output, target_labels)
+            total_loss += loss_train.item()
+            batch_accuracy_identity, batch_accuracy_gender, batch_accuracy_age = \
+                calculate_metrics(output, target_labels)
+
+            accuracy_identity += batch_accuracy_identity
+            accuracy_gender += batch_accuracy_gender
+            accuracy_age += batch_accuracy_age
+
+            loss_train.backward()
+            optimizer.step()
+
+        if not args.warmup:
+            lr_scheduler.step()  # Use this in case warm-up learning strategy is not used!
+
+        # print('lr: ', optimizer.param_groups[0]['lr'], optimizer.param_groups[1]['lr'])
+
+        print("epoch {:4d}, loss: {:.4f}, identity: {:.4f}, gender: {:.4f}, age: {:.4f}".format(
+            epoch,
+            total_loss / n_train_samples,
+            accuracy_identity / n_train_samples,
+            accuracy_gender / n_train_samples,
+            accuracy_age / n_train_samples))
+
+        logger.add_scalar('train_loss', total_loss / n_train_samples, epoch)
+
+        if epoch % 5 == 0:
+            # validate(model, val_dataloader, logger, epoch, device)
+            accuracy_val_identity, accuracy_val_gender, accuracy_val_age = \
+                validate(model, val_dataloader, logger, epoch, device)
+
+            average_accuracy = (accuracy_val_identity + accuracy_val_gender + accuracy_val_age) / 3  # Average accuracy
+
+            if average_accuracy > best_accuracy:
+                best_model_wts = copy.deepcopy(model.state_dict())
+                best_epoch = epoch
+                best_accuracy = average_accuracy
+                each_at_best_accuracy = [accuracy_val_identity, accuracy_val_gender, accuracy_val_age]
+
+        if epoch % 10 == 0:
+            checkpoint_save(model, savedir, epoch)
+
+    if best_epoch >= 5:
+        # Print best validation accuracy
+        print('Best validation average Accuracy: {:4f}, identity: {:.4f}, gender: {:.4f}, age: {:.4f}'.format(
+            best_accuracy, each_at_best_accuracy[0], each_at_best_accuracy[1], each_at_best_accuracy[2]))
+
+        # Load best model weights
+        model.load_state_dict(best_model_wts)
+        checkpoint_save(model, savedir, best_epoch, True)
+
+    logger.close()
+
+    print('Training is finished!')
